f418eeae062a2912eb354527157f7aa739459ec3
foureverhh <foureverhh@gmail.com>
2018-02-09 22:02:06 +0100

    Random of letters does not work well yet

--- app/src/main/java/com/nackademin/foureverhh/sfiapplication180113/WordPuzzle.java
+++ app/src/main/java/com/nackademin/foureverhh/sfiapplication180113/WordPuzzle.java
@@ -1,5 +1,6 @@
 package com.nackademin.foureverhh.sfiapplication180113;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -18,8 +19,6 @@ public class WordPuzzle extends AppCompatActivity {
     String[] buttonLetters;
     String[] threeLetters;
     char rightLetter;
-    boolean imageButtonIsClicked;
-    boolean next;
 
 
     @Override
@@ -34,50 +33,21 @@ public class WordPuzzle extends AppCompatActivity {
         btn1 = findViewById(R.id.btn_choice1);
         btn2 = findViewById(R.id.btn_choice2);
         btn3 = findViewById(R.id.btn_choice3);
+        game();
+    }
 
-        // to wait until nextWord allow the game to go on
+    public void game()
+    {
+
         rightLetter = wordMatching.takeRightLetter(wordMatching.svWord);
-        Log.d("The right letter is ", String.valueOf(rightLetter));
-        Log.d("The Swedish word",wordMatching.svWord);
-
-        //wordMatching.counter();
 
         //To show the English word
         txv_en.setText(wordMatching.enWord);
 
         //To show the Swedish word with an empty blank
         txv_sv.setText(wordMatching.makeEmptyPlace(rightLetter, wordMatching.svWord));
-        Log.d("Empty sv word", wordMatching.makeEmptyPlace(rightLetter, wordMatching.svWord));
-        do {
-            game();
-        }while(next);
-       /* btn1.setOnClickListener(this);
-        btn2.setOnClickListener(this);
-        btn3.setOnClickListener(this);
-        nextWord.setOnClickListener(this);*/
-    }
-    public void game()
-    {
-        //while(wordMatching.getCounter()<wordMatching.sv.length)
 
-            next=false;
-           /* // to wait until nextWord allow the game to go on
-            rightLetter = wordMatching.takeRightLetter(wordMatching.svWord);
-            Log.d("The right letter is ", String.valueOf(rightLetter));
-            Log.d("The Swedish word",wordMatching.svWord);
-
-            //wordMatching.counter();
-
-            //To show the English word
-            txv_en.setText(wordMatching.enWord);
-
-            //To show the Swedish word with an empty blank
-            txv_sv.setText(wordMatching.makeEmptyPlace(rightLetter, wordMatching.svWord));
-            Log.d("Empty sv word", wordMatching.makeEmptyPlace(rightLetter, wordMatching.svWord));*/
-
-
-
-            //Get three letters for the 3 buttons
+        //Get three letters for the 3 buttons
             threeLetters
                     = wordMatching.getAllThreeLetters(rightLetter, wordMatching.alphabet);
             for (String str : threeLetters)
@@ -120,32 +90,9 @@ public class WordPuzzle extends AppCompatActivity {
                 }
             });
 
-            nextWord.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    if (wordMatching.getCounter() < wordMatching.sv.length) {
-                        imageButtonIsClicked = true;
-                        wordMatching.counterPlus();
-                        wordMatching.enWord = wordMatching.en[wordMatching.getCounter()];
-                        wordMatching.svWord = wordMatching.sv[wordMatching.getCounter()];
-                        next=true;
-                    } else {
-                        imageButtonIsClicked = false;
-                        next = false;
-                    }
-
-                    Log.d("Txv_en ", txv_en.getText().toString());
-                    Log.d("Txv_sv ", txv_sv.getText().toString());
-                    nextWord.setVisibility(View.INVISIBLE);
-                    Log.d("Bool after image", String.valueOf(next));
-                }
-            });
-
-
-
-            //wordMatching.counterPlus();
-            Log.d("The boolean is",String.valueOf(next));
-
+        btn1.setBackgroundColor(Color.LTGRAY);
+        btn2.setBackgroundColor(Color.LTGRAY);
+        btn3.setBackgroundColor(Color.LTGRAY);
     }
 
 
@@ -156,109 +103,43 @@ public class WordPuzzle extends AppCompatActivity {
         if(buttonLetter.equals(rLetter))
         {
             btnChosen.setBackgroundColor(Color.GREEN);
-            btnWrong1.setBackgroundColor(Color.GRAY);
-            btnWrong2.setBackgroundColor(Color.GRAY);
+            btnWrong1.setBackgroundColor(Color.LTGRAY);
+            btnWrong2.setBackgroundColor(Color.LTGRAY);
             nextWord.setVisibility(View.VISIBLE);
-            Log.d("Counter in next",String.valueOf(wordMatching.getCounter()));
+            String result = getResources().getString(R.string.rightResult);
+            txv_result.setText(result);
 
 
         }else
         {
             btnChosen.setBackgroundColor(Color.RED);
-            btnWrong1.setBackgroundColor(Color.GRAY);
-            btnWrong2.setBackgroundColor(Color.GRAY);
+            btnWrong1.setBackgroundColor(Color.LTGRAY);
+            btnWrong2.setBackgroundColor(Color.LTGRAY);
             nextWord.setVisibility(View.INVISIBLE);
-
+            String result = getResources().getString(R.string.wrongResult);
+            txv_result.setText(result);
         }
     }
-      /* public void button1Click(View v)
-        {
-            String btnChoice = btn1.getText().toString().toLowerCase();
-            Log.d("Button 1 letter is :",btnChoice);
-            getRightAnswer(btnChoice,String.valueOf(rightLetter),btn1,btn2,btn3);
-        }
 
-         public void button2Click(View v)
+         public void nextWord(View v)
          {
-            String btnChoice = btn2.getText().toString().toLowerCase();
-            Log.d("Button 2 letter is :",btnChoice);
-            getRightAnswer(btnChoice,String.valueOf(rightLetter),btn2,btn1,btn3);
-         }
-
-         public void button3Click(View v)
-         {
-            String btnChoice = btn3.getText().toString().toLowerCase();
-            Log.d("Button 3 letter is :",btnChoice);
-            getRightAnswer(btnChoice,String.valueOf(rightLetter),btn3,btn1,btn2);
-         }
-
-         public void counterRise(View v)
-         {
-
-
-
              //To make text_en and text_sv have new content
              wordMatching.counterPlus();
 
              if(wordMatching.getCounter()<wordMatching.sv.length) {
-                 next = true;
-
                  wordMatching.enWord =wordMatching.en[wordMatching.getCounter()];
                  wordMatching.svWord = wordMatching.sv[wordMatching.getCounter()];
+                 game();
+                 nextWord.setVisibility(View.INVISIBLE);
+                 txv_result.setText(" ");
              }else
-                 next =false;
-             Log.d("Txv_en ",txv_en.getText().toString());
-             Log.d("Txv_sv ",txv_sv.getText().toString());
-             nextWord.setVisibility(View.INVISIBLE);
-             Log.d("Bool after image",String.valueOf(next));
+             {
+                 String result = getResources().getString(R.string.gameOver);
+                 txv_result.setText(result);
+                 Intent intent = new Intent(this, MainActivity.class);
+                 startActivity(intent);
+             }
          }
-*/
 
-       /*@Override
-    public void onClick(View v)
-    {
-        String btnChoice;
-        switch (v.getId())
-        {
-            case R.id.btn_choice1:
-                btnChoice = btn1.getText().toString().toLowerCase();
-                Log.d("Button 1 letter is :",btnChoice);
-                getRightAnswer(btnChoice,String.valueOf(rightLetter),btn1,btn2,btn3);
-                break;
-
-            case R.id.btn_choice2:
-                btnChoice = btn2.getText().toString().toLowerCase();
-                Log.d("Button 2 letter is :",btnChoice);
-                getRightAnswer(btnChoice,String.valueOf(rightLetter),btn2,btn1,btn3);
-                break;
-
-            case R.id.btn_choice3:
-                btnChoice = btn3.getText().toString().toLowerCase();
-                Log.d("Button 3 letter is :",btnChoice);
-                getRightAnswer(btnChoice,String.valueOf(rightLetter),btn3,btn1,btn2);
-                break;
-
-            case R.id.imageButton:
-                wordMatching.counterPlus();
-                next = true;
-                nextWord.setVisibility(View.INVISIBLE);
-                //To make text_en and text_sv have new content
-                if(wordMatching.getCounter()<wordMatching.sv.length) {
-                    wordMatching.enWord =wordMatching.en[wordMatching.getCounter()];
-                    wordMatching.svWord = wordMatching.sv[wordMatching.getCounter()];
-                }
-                rightLetter = wordMatching.takeRightLetter(wordMatching.svWord);
-
-                //To show the English word
-                txv_en.setText(wordMatching.enWord);
-
-                //To show the Swedish word with an empty blank
-                txv_sv.setText(wordMatching.makeEmptyPlace(rightLetter, wordMatching.svWord));
-
-
-                break;
-
-        }
-    }*/
 }
 
